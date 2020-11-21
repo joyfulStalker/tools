901594108653986fabf20075d49696f2aed7d891
joyfulStalker <joyfulStalker@qq.com>
2020-11-21 11:55:57 +0800

    swagger2word

---
+++ swagger2word/src/main/java/co/imdo/perfect/service/OpenApiWordService.java
@@ -0,0 +1,17 @@
+package co.imdo.perfect.service;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.Map;
+
+/**
+ * Created by XiuYin.Cui on 2018/1/12.
+ */
+public interface OpenApiWordService {
+
+    Map<String,Object> tableList(String swaggerUrl);
+
+    Map<String, Object> tableListFromString(String jsonStr);
+
+    Map<String, Object> tableList(MultipartFile jsonFile);
+}
