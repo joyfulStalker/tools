8946c66cf48fe05d25a71ed5c918d67943fed4b6
joyfulStalker <joyfulStalker@qq.com>
2020-11-21 14:15:24 +0800

    git忽略

--- batch-import-to-mysql-from-excel/target/classes/sql/mytable.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE `batchImportFromExcel` (
-  `uid` bigint(15) NOT NULL AUTO_INCREMENT,
-  `uname` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL,
-  `uphone` varchar(11) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL,
-  `uaddress` varchar(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL,
-  PRIMARY KEY (`uid`),
-  KEY `index_name` (`uname`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
